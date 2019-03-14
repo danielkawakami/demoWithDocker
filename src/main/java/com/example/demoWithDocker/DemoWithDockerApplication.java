9fe5828779085ab512727d48268ffb2b832e8650
danielkawakami <danielkawakamitmp-contatos@yahoo.com.br>
2019-03-14 17:16:19 -0300

    Aplicação funcionando no docker, vide UsoDockerNoTerminal.txt

---
+++ src/main/java/com/example/demoWithDocker/DemoWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoWithDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWithDockerApplication.class, args);
+	}
+
+}
